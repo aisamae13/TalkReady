5e3388be4a2801d8a3137e6505aecec49194e812
Alyssa <alyssamaebahalla072@gmail.com>
2025-05-02 01:18:19 +0800

    update update 02/05/2025

--- talkready_mobile/lib/splash_screen.dart
+++ talkready_mobile/lib/splash_screen.dart
@@ -11,7 +11,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 4), () {
       if (mounted) {
         Navigator.pushReplacementNamed(context, '/');
       }
